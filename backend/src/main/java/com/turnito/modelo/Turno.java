80acb080ace9cb1adf5870a5925ad1f2952b7057
marki0 <markitosjr27@gmail.com>
2025-05-02 18:26:35 -0300

    Creacion datos turno

--- backend/src/main/java/com/turnito/modelo/Turno.java
+++ backend/src/main/java/com/turnito/modelo/Turno.java
@@ -1,4 +1,15 @@
 package com.turnito.modelo;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Turno {
+    private int Id;
+    private LocalDate fecha;
+    private LocalTime hora;
+    private boolean estado;
+    private String direccion;
+    private int profesional_id;
+    private int servicio_id;
+    private int solicitante_id;
 }
